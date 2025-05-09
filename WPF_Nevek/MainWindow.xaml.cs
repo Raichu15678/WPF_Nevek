931ca39bdd567a6b4b7eeac52a1a3a284e8c26a3
Raichu15678 <matefekete355@gmail.com>
2025-05-09 08:03:13 +0200

    Add project files.

---
+++ WPF_Nevek/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WPF_Nevek
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<string> Nevek = new List<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_nevekkiir_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbx_nevekbeir.Text != "")
+            {
+                lb_nevekkiir.Items.Clear();
+                Nevek.Clear();
+                string Nevszovegdoboz = tbx_nevekbeir.Text;
+                string[] kecske = Nevszovegdoboz.Split(',');
+                foreach (string s in kecske)
+                {
+                    Nevek.Add(s);
+                    lb_nevekkiir.Items.Add(s);
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Kérem adjon meg egy nevet", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void btn_rendez_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbx_nevekbeir.Text != "")
+            {
+                lb_nevekrendezve.Items.Clear();
+                Nevek.Clear();
+                string Nevszovegdoboz = tbx_nevekbeir.Text;
+                string[] kecske = Nevszovegdoboz.Split(',');
+                Array.Sort(kecske);
+                foreach (string s in kecske)
+                {
+                    Nevek.Add(s);
+                    lb_nevekrendezve.Items.Add(s);
+                }
+                MessageBox.Show("A név lista sorba lett rendezve :) ", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Kérem adjon meg egy nevet", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
